4756c7435739ad6ef170e1d6d6b1b9452f943bb0
Andrej Szalma <andrej@fortytwo.studio>
2020-02-06 16:48:56 +0000

    edited styles

--- public/app.js
+++ public/app.js
@@ -13,4 +13,9 @@ $( document ).ready(function() {
         $('#login_wrap').removeClass('active');        
     }
     
+    $('a').each(function(){
+        if ($(this).prop('href') == window.location.href) {
+            $(this).addClass('active'); $(this).parents('li').addClass('active');
+        }
+    });
 });
